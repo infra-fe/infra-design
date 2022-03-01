ed54fb53d4d4084bc43a3396a7322e192149db0f
zhang lay <lay.zhang@shopee.com>
2022-03-01 12:03:28 +0800

    fix: Optimize the writing of axios

--- scripts/notice-seaTalk.js
+++ scripts/notice-seaTalk.js
@@ -4,22 +4,23 @@ const axios = require('axios');
 const cwd = process.cwd();
 
 const { version } = require(path.resolve(cwd, 'package.json'));
+const seatalkUrl = 'https://openapi.seatalk.io/webhook/group/_mh_SKGeQruZ49eX4CzqQw';
 
 function noticeSeaTalk() {
-  axios({
-    url: 'https://openapi.seatalk.io/webhook/group/_mh_SKGeQruZ49eX4CzqQw',
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-    },
-    data: {
+  axios
+    .post(seatalkUrl, {
       tag: 'text',
       text: {
         content: `Hello, everyone! Infrad has released version ${version}, Please check the update details from https://bolifestudio.com/changelog-cn`,
         at_all: true,
       },
-    },
-  });
+    })
+    .then(res => {
+      console.log('Successful notice in seatalk.');
+    })
+    .catch(e => {
+      console.log(e);
+    });
 }
 
 noticeSeaTalk();
