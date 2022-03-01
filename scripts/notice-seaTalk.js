f9c660ca2c825e090b54d233f7952bec82bf0628
zhang lay <lay.zhang@shopee.com>
2022-03-01 11:43:00 +0800

    fix: use axios instead of request

--- scripts/notice-seaTalk.js
+++ scripts/notice-seaTalk.js
@@ -1,19 +1,18 @@
 const path = require('path');
-const request = require('request');
+const axios = require('axios');
 
 const cwd = process.cwd();
 
 const { version } = require(path.resolve(cwd, 'package.json'));
 
 function noticeSeaTalk() {
-  request({
+  axios({
     url: 'https://openapi.seatalk.io/webhook/group/_mh_SKGeQruZ49eX4CzqQw',
     method: 'POST',
-    json: true,
     headers: {
       'content-type': 'application/json',
     },
-    body: {
+    data: {
       tag: 'text',
       text: {
         content: `Hello, everyone! Infrad has released version ${version}, Please check the update details from https://bolifestudio.com/changelog-cn`,
