01255f59cf21f768da3dcc15af343d1c5f2f1c47
zhanglei66 <38252062+zhanglei66@users.noreply.github.com>
2022-03-01 12:55:37 +0800

    Feature v4.18.5 rollback (#53)
    * fix: use axios instead of request
    
    * fix: Optimize the writing of axios
    
    Co-authored-by: zhang lay <lay.zhang@shopee.com>

--- scripts/notice-seaTalk.js
+++ scripts/notice-seaTalk.js
@@ -1,26 +1,26 @@
 const path = require('path');
-const request = require('request');
+const axios = require('axios');
 
 const cwd = process.cwd();
 
 const { version } = require(path.resolve(cwd, 'package.json'));
+const seatalkUrl = 'https://openapi.seatalk.io/webhook/group/_mh_SKGeQruZ49eX4CzqQw';
 
 function noticeSeaTalk() {
-  request({
-    url: 'https://openapi.seatalk.io/webhook/group/_mh_SKGeQruZ49eX4CzqQw',
-    method: 'POST',
-    json: true,
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: {
+  axios
+    .post(seatalkUrl, {
       tag: 'text',
       text: {
         content: `Hello, everyone! Infrad has released version ${version}, Please check the update details from https://bolifestudio.com/changelog-cn`,
         at_all: true,
       },
-    },
-  });
+    })
+    .then(res => {
+      console.log('Successful notice in seatalk.');
+    })
+    .catch(e => {
+      console.log(e);
+    });
 }
 
 noticeSeaTalk();
